a240d55b84b73c9ff7ab268e9b8f97608848f35f
AngryShrimp <johnstone.keenan@gmail.com>
2021-02-11 13:24:01 -0600

    two whole ass servers

---
+++ ddsServer/server-start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -Xms1024M -Xmx14G -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
---
+++ ddsServer/serverstart.bat
@@ -0,0 +1,2 @@
+java -Xms1024M -Xmx6048M -jar forge-1.12.2-14.23.5.2847-universal.jar nogui
+pause
